9dceac412b03beb9aee4722b58df44c4c58e6ad2
Daniel Zatorski <zatorskidaniel@gmail.com>
2024-08-20 17:41:42 +0200

    Update 4.Employee_details_by_hiring_date.sql

--- SQL_queries/4.Employee_details_by_hiring_date.sql
+++ SQL_queries/4.Employee_details_by_hiring_date.sql
@@ -1,9 +1,9 @@
 SELECT 
 e.first_name, -- first name of the employee from the employees table
 e.last_name, -- last name of the employee from the employees table
+e.hire_date -- hire date of the employee from the employees table
 t.title, -- job title of the employee from the titles table
 s.salary, -- current salary of the employee from the salaries table
-e.hire_date -- hire date of the employee from the employees table
 FROM employees e -- the employees table, which contains basic employee information
 
 JOIN salaries s ON e.emp_no = s.emp_no -- join to get salary details by matching employee numbers between employees and salaries tables
